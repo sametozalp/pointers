3d9b3bd7d9569296a744c450eb425a90dc01357f
Samet Özalp <93615071+sametozalp@users.noreply.github.com>
2024-07-16 19:12:24 +0300

    Initial commit

---
+++ pointers/ChangeNumbersValue.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+
+void changeValue(int* x, int* y) {
+	int temp;
+	temp = *x;
+	*x = *y;
+	*y = temp;
+}
+
+int main() {
+
+
+	int a, b;
+
+	a = 5; b = 8;
+
+	std::cout << a << " " << b << std::endl;
+
+	changeValue(&a, &b);
+
+	std::cout << a << " " << b << std::endl;
+
+
+	return 0;
+}
